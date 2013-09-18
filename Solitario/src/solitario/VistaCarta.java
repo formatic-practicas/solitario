9fe4498bb672d504ed5713ddf03022a6c766f2be
formatic-practicas <formatic@mundo-r.com>
2013-09-18 08:55:43 +0200

    Arrastra correctamente

--- Solitario/src/solitario/VistaCarta.java
+++ Solitario/src/solitario/VistaCarta.java
@@ -6,12 +6,17 @@ import javax.swing.JPanel;
 
 public class VistaCarta extends JPanel {
 	public VistaCarta(Carta carta){
+		this.carta = carta;
 		setBackground(java.awt.Color.WHITE);		
 		JLabel l = new JLabel(carta.toString());
 		if(carta.getColor() == Color.ROJO){
 			l.setForeground(java.awt.Color.RED);
 		}
 		add(l);
-		setSize(250, 50);
+		setSize(50, 50);
+	}
+	Carta carta;
+	public Carta getCarta(){
+		return carta;
 	}
 }
--- Solitario/src/solitario/VistaMesa.java
+++ Solitario/src/solitario/VistaMesa.java
@@ -1,6 +1,5 @@
 package solitario;
 
-
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -10,54 +9,29 @@ import javax.swing.JPanel;
 
 public class VistaMesa extends JPanel {
 
-	private VistaMazo selected;
-	public VistaMesa(Mesa mesa){
+	private VistaCarta selected;
+	JPanel panelCartas, panelMazos;
+
+	public VistaMesa(Mesa mesa) {
 		this.mesa = mesa;
+		setSize(400, 400);
+		panelCartas = new JPanel();
+		panelCartas.setLayout(null);
+		panelCartas.setBounds(getBounds());
+		panelMazos = new JPanel();
+		panelMazos.setLayout(null);
+		panelMazos.setBounds(getBounds());
 		setLayout(null);
-		
-		VistaMazo vistaBaraja = new VistaMazo(mesa.mazos[TipoMazo.BARAJA.ordinal()]);
-		add(vistaBaraja);
-		vistaBaraja.setLocation(0,5);
-		
-		VistaMazo vistaDescarte = new VistaMazo(mesa.mazos[TipoMazo.DESCARTE.ordinal()]);
-		add(vistaDescarte);
-		vistaDescarte.setLocation(60,5);	
+		add(panelCartas);
+		panelCartas.setOpaque(false);
+		add(panelMazos);
 
-		VistaMazo final1 = new VistaMazo(mesa.mazos[TipoMazo.FINAL1.ordinal()]);
-		final1.setLocation(120,5);
-		add(final1);
-		
-		VistaMazo final2 = new VistaMazo(mesa.mazos[TipoMazo.FINAL2.ordinal()]);
-		final2.setLocation(182, 5);
-		add(final2);
-		
-		VistaMazo final3 = new VistaMazo(mesa.mazos[TipoMazo.FINAL3.ordinal()]);
-		final3.setLocation(241, 5);
-		add(final3);
-		
-		VistaMazo final4 = new VistaMazo(mesa.mazos[TipoMazo.FINAL4.ordinal()]);
-		final4.setLocation(301, 5);
-		add(final4);
-
-		VistaMazo intermedio1 = new VistaMazo(mesa.mazos[TipoMazo.INTERMEDIO1.ordinal()]);
-		intermedio1.setLocation(120,75);
-		add(intermedio1);
-		
-		VistaMazo intermedio2 = new VistaMazo(mesa.mazos[TipoMazo.INTERMEDIO2.ordinal()]);
-		intermedio2.setLocation(182, 75);
-		add(intermedio2);
-		
-		VistaMazo intermedio3 = new VistaMazo(mesa.mazos[TipoMazo.INTERMEDIO3.ordinal()]);
-		intermedio3.setLocation(241, 75);
-		add(intermedio3);
-		
-		VistaMazo intermedio4 = new VistaMazo(mesa.mazos[TipoMazo.INTERMEDIO4.ordinal()]);
-		intermedio4.setLocation(301, 75);
-		add(intermedio4);
+		colocarMazos(mesa);
+		colocarCartas(mesa);
 		addMouseMotionListener(new MouseAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				if(selected!= null){
+				if (selected != null) {
 					selected.setLocation(e.getPoint());
 				}
 			}
@@ -65,25 +39,134 @@ public class VistaMesa extends JPanel {
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				Component c = VistaMesa.this.getComponentAt(e.getPoint());
-				if(c!= null && c != VistaMesa.this){
-					selected = (VistaMazo)c;
+				Component c = VistaMesa.this.panelCartas.getComponentAt(e
+						.getPoint());
+				if (c != null && c != VistaMesa.this.panelCartas) {
+					selected = (VistaCarta) c;
 				}
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
+				Component c = VistaMesa.this.panelMazos.getComponentAt(e
+						.getPoint());
+				if (c != null && c != VistaMesa.this.panelMazos) {
+					VistaMazo vistaMazo = (VistaMazo) c;
+					if (selected != null) {
+						Carta carta = selected.getCarta();
+						Mazo old = carta.getMazo();
+						if (vistaMazo.mazo.agregarCarta(carta)) {
+							old.extraerCarta();
+						}
+					}
+				}
+				VistaMesa.this.colocarCartas(VistaMesa.this.mesa);
 				selected = null;
 			}
-		});	
+		});
 	}
-	
+
+	private void colocarCartas(Mesa mesa) {
+		panelCartas.removeAll();
+		panelCartas.repaint();
+		
+		if (!mesa.mazos[TipoMazo.DESCARTE.ordinal()].estaVacio()) {
+			VistaCarta vistaCartaDescarte = new VistaCarta(
+					mesa.mazos[TipoMazo.DESCARTE.ordinal()].getUltimaCarta());
+			panelCartas.add(vistaCartaDescarte);
+			vistaCartaDescarte.setLocation(60, 5);
+		}
+
+		if (!mesa.mazos[TipoMazo.INTERMEDIO1.ordinal()].estaVacio()) {
+			VistaCarta intermedio1 = new VistaCarta(
+					mesa.mazos[TipoMazo.INTERMEDIO1.ordinal()].getUltimaCarta());
+			intermedio1.setLocation(120, 75);
+			panelCartas.add(intermedio1);
+		}
+
+		if (!mesa.mazos[TipoMazo.INTERMEDIO2.ordinal()].estaVacio()) {
+			VistaCarta intermedio2 = new VistaCarta(
+					mesa.mazos[TipoMazo.INTERMEDIO2.ordinal()].getUltimaCarta());
+			intermedio2.setLocation(182, 75);
+			panelCartas.add(intermedio2);
+		}
+
+		if (!mesa.mazos[TipoMazo.INTERMEDIO3.ordinal()].estaVacio()) {
+			VistaCarta intermedio3 = new VistaCarta(
+					mesa.mazos[TipoMazo.INTERMEDIO3.ordinal()].getUltimaCarta());
+			intermedio3.setLocation(241, 75);
+			panelCartas.add(intermedio3);
+		}
+
+		if (!mesa.mazos[TipoMazo.INTERMEDIO4.ordinal()].estaVacio()) {
+			VistaCarta intermedio4 = new VistaCarta(
+					mesa.mazos[TipoMazo.INTERMEDIO4.ordinal()].getUltimaCarta());
+			intermedio4.setLocation(301, 75);
+			panelCartas.add(intermedio4);
+		}
+
+		panelCartas.revalidate();
+		panelCartas.repaint();
+
+	}
+
+	private void colocarMazos(Mesa mesa) {
+		VistaMazo vistaBaraja = new VistaMazo(
+				mesa.mazos[TipoMazo.BARAJA.ordinal()]);
+		panelMazos.add(vistaBaraja);
+		vistaBaraja.setLocation(0, 5);
+
+		VistaMazo vistaDescarte = new VistaMazo(
+				mesa.mazos[TipoMazo.DESCARTE.ordinal()]);
+		panelMazos.add(vistaDescarte);
+		vistaDescarte.setLocation(60, 5);
+
+		VistaMazo final1 = new VistaMazo(mesa.mazos[TipoMazo.FINAL1.ordinal()]);
+		final1.setLocation(120, 5);
+		panelMazos.add(final1);
+
+		VistaMazo final2 = new VistaMazo(mesa.mazos[TipoMazo.FINAL2.ordinal()]);
+		final2.setLocation(182, 5);
+		panelMazos.add(final2);
+
+		VistaMazo final3 = new VistaMazo(mesa.mazos[TipoMazo.FINAL3.ordinal()]);
+		final3.setLocation(241, 5);
+		panelMazos.add(final3);
+
+		VistaMazo final4 = new VistaMazo(mesa.mazos[TipoMazo.FINAL4.ordinal()]);
+		final4.setLocation(301, 5);
+		panelMazos.add(final4, 0);
+
+		VistaMazo intermedio1 = new VistaMazo(
+				mesa.mazos[TipoMazo.INTERMEDIO1.ordinal()]);
+		intermedio1.setLocation(120, 75);
+		panelMazos.add(intermedio1);
+
+		VistaMazo intermedio2 = new VistaMazo(
+				mesa.mazos[TipoMazo.INTERMEDIO2.ordinal()]);
+		intermedio2.setLocation(182, 75);
+		panelMazos.add(intermedio2);
+
+		VistaMazo intermedio3 = new VistaMazo(
+				mesa.mazos[TipoMazo.INTERMEDIO3.ordinal()]);
+		intermedio3.setLocation(241, 75);
+		panelMazos.add(intermedio3);
+
+		VistaMazo intermedio4 = new VistaMazo(
+				mesa.mazos[TipoMazo.INTERMEDIO4.ordinal()]);
+		intermedio4.setLocation(301, 75);
+		panelMazos.add(intermedio4);
+	}
+
 	Mesa mesa;
+
 	public static void main(String[] args) {
 		Mesa mesa = new Mesa();
 		VistaMesa v = new VistaMesa(mesa);
 		JFrame f = new JFrame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.getContentPane().add(v);
-		f.setSize(400,400);
+		f.setSize(400, 400);
 		f.setVisible(true);
 	}
 }
